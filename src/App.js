29d2ae9fd100742e03bd5f5933900d04bcf59cdc
rivaldo-maciel <rivaldomaciel@hotmail.com>
2022-03-06 22:07:20 -0300

    install Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <h1>Amazing-Shop</h1>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={() => {}} />
+        <Route path="/cart" component={() => {}} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
