5cb7a6af869f1ef68af4c9aa031f1422e5792434
rivaldo-maciel <rivaldomaciel@hotmail.com>
2022-03-08 12:55:07 -0300

    uninstall Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" component={() => {}} />
+        <Route exact path="/" component={Home} />
         <Route path="/cart" component={() => {}} />
       </Switch>
     </BrowserRouter>
