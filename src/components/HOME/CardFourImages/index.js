bba9179f96d835aa1acf5c308fc91b4f38fda01a
rivaldo-maciel <rivaldomaciel@hotmail.com>
2022-11-08 09:58:05 -0300

    refactor: refact home products cards

--- src/components/HOME/CardFourImages/index.js
+++ src/components/HOME/CardFourImages/index.js
@@ -2,23 +2,23 @@ import React from 'react';
 
 function CardFourImages({ infos }) {
   return (
-    <div className="bg-white w-[300px] h-[320px] text-[12px] flex justify-center items-center">
-      <div className="grid grid-cols-[90px_90px] w-[180px] justify-center gap-2">
+    <div className="bg-white w-[19rem] h-[19rem] text-[0.7rem] flex justify-center items-center cursor-pointer">
+      <div className="grid grid-cols-[5.7rem_5.7rem] w-[11rem] justify-center gap-2">
         <div className="col-span-2 text-[14px] mb-2 font-bold">{infos.title}</div>
         <div>
-          <img className="w-[90px]" src={infos.img1} alt="offers" />
+          <img className="w-[5.7rem]" src={infos.img1} alt="offers" />
           <p>{infos.subtitle1}</p>
         </div>
         <div>
-          <img className="w-[90px]" src={infos.img2} alt="offers" />
+          <img className="w-[5.7rem]" src={infos.img2} alt="offers" />
           <p>{infos.subtitle2}</p>
         </div>
         <div>
-          <img className="w-[90px]" src={infos.img3} alt="offers" />
+          <img className="w-[5.7rem]" src={infos.img3} alt="offers" />
           <p>{infos.subtitle3}</p>
         </div>
         <div>
-          <img className="w-[90px]" src={infos.img4} alt="offers" />
+          <img className="w-[5.7rem]" src={infos.img4} alt="offers" />
           <p>{infos.subtitle4}</p>
         </div>
       </div>
