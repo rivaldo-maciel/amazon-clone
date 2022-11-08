8a69dc6e50652f137acc13da9e6486327ebbd5c3
rivaldo-maciel <rivaldomaciel@hotmail.com>
2022-11-08 10:12:44 -0300

    refact: refact footer styles

--- src/components/FOOTER/Footer/index.js
+++ src/components/FOOTER/Footer/index.js
@@ -6,14 +6,14 @@ import CountriesFooter from '../CountriesFooter';
 
 function Footer() {
   return (
-    <footer className="h-[470px] mt-[50px] text-white">
+    <footer className="h-[10rem] mt-[4rem] text-white">
       <a href="#top">
-        <div className="bg-[#37475A] h-[45px] flex justify-center items-center hover:bg-[#435164] cursor-pointer">
-        <span className="text-[8px]">Voltar ao início</span>
+        <div className="bg-[#37475A] h-[4rem] flex justify-center items-center hover:bg-[#435164] cursor-pointer">
+        <span className="text-[0.8rem]">Voltar ao início</span>
       </div>
       </a>
-      <div className="bg-[#232F3E] h-[300px] flex justify-center items-center">
-        <div className="h-[240px] w-[600px] flex justify-evenly">
+      <div className="bg-[#232F3E] h-[19rem] flex justify-center items-center">
+        <div className="h-[10rem] w-[38rem] flex justify-evenly">
           <LinksFooterUl infos={linksFooter.meetUs} />
           <LinksFooterUl infos={linksFooter.earnyMoney} />
           <LinksFooterUl infos={linksFooter.letUsHelp} />
@@ -24,13 +24,13 @@ function Footer() {
         <div className="bg-[#232F3E] flex justify-center">
           <div className="flex flex-col justify-center">
             <a href="/" className="self-center">
-              <img className="h-[30px] mt-[10px]" src={Logo} alt="amazing" />
+              <img className="h-[2rem] mt-[0.8rem]" src={Logo} alt="amazing" />
             </a>
             <CountriesFooter />
           </div>
         </div>
-      <div className="bg-[#131A22] h-[160px] flex justify-center items-center">
-        <p>Aplicação feita unicamente para fins de estudos, todos os direitos reservados á: amazon.com.br e mercadolivre.com.br</p>
+      <div className="bg-[#131A22] h-[10rem] flex justify-center items-center">
+        <p>Aplicação feita unicamente para fins de estudos, todos os direitos reservados à: amazon.com.br e mercadolivre.com.br</p>
       </div>
     </footer>
   );
