eb1bb2763e5e84a1799b6d4450d84e00d38b6e46
rivaldo-maciel <rivaldomaciel@hotmail.com>
2022-11-09 10:37:36 -0300

    feat: add NavBar on Header

---
+++ src/components/NavBar/links.js
@@ -0,0 +1,12 @@
+export const links = [
+  "Todos",
+  "Venda na Amazon",
+  "Mais Vendidos",
+  "Esquenta Black Friday",
+  "Prime",
+  "Atendimento ao Cliente",
+  "Música",
+  "Livros",
+  "Novidades na Amazon",
+  "Eletrônicos"
+]
